3444b03abeb0da9a7e3f44f2717e606a1e4f926f
Ash5021314 <ashkapoghosyan@gmail.com>
2020-03-10 01:46:21 +0400

    removing class

--- client/src/Components/Items.jsx
+++ client/src/Components/Items.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import Container from 'react-bootstrap/Container'
 import './Items.css'
 import doors from '../doors'
@@ -7,10 +7,10 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-import { connect } from "react-redux";
-import { getInteriorDoors, getIronDoors } from "../store/actions/doorsAction";
+import {connect} from 'react-redux'
+import {getInteriorDoors, getIronDoors} from '../store/actions/doorsAction'
 
 const Items = (props) => {
   const [ironDoor, setIronDoors] = useState({})
@@ -29,52 +29,52 @@ const Items = (props) => {
   })
 
   useEffect(() => {
-    props.getInteriorDoors(0, 4);
-    props.getIronDoors(0, 4);
-  }, []);
+    props.getInteriorDoors(0, 4)
+    props.getIronDoors(0, 4)
+  }, [])
 
   useEffect(() => {
     setIronDoors(props.doors.iron)
     setInteriorDoors(props.doors.interior)
-  }, [props.doors]);
+  }, [props.doors])
   return (
     <>
       <Container>
         <h2 className="prodHeader"> Входные двери</h2>
         <CardDeck>
           {!ironDoor.length ? <h2>Loading...</h2> : (
-            <Row style={{ width: '110%' }}>
+            <Row style={{width: '110%'}}>
               {
                 ironDoor.map((res, index) => {
                   return (<Col xs={6} md={3} key={index}>
-                    <Card>
-                      <div className="flip-box">
-                        <div className="flip-box-inner">
-                          <div className="flip-box-front">
-                            <Card.Img variant="top" src={res.frontImage}
-                              style={{ height: '100%' }} />
-                          </div>
-                          <div className="flip-box-back">
-                            <Card.Img variant="top" src={res.backImage}
-                              style={{ height: '100%' }} />
+                      <Card>
+                        <div className="flip-box">
+                          <div className="flip-box-inner">
+                            <div className="flip-box-front">
+                              <Card.Img variant="top" src={res.frontImage}
+                                        style={{height: '100%'}}/>
+                            </div>
+                            <div className="flip-box-back">
+                              <Card.Img variant="top" src={res.backImage}
+                                        style={{height: '100%'}}/>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <Card.Body className="alignCenter">
-                        <Card.Title>{res.title}</Card.Title>
-                      </Card.Body>
-                      <Card.Footer>
-                        <div className="text-muted alignCenter ">{res.price} <span>р.</span></div>
-                      </Card.Footer>
-                    </Card>
-                  </Col>
+                        <Card.Body className="alignCenter">
+                          <Card.Title>{res.title}</Card.Title>
+                        </Card.Body>
+                        <Card.Footer>
+                          <div className="text-muted alignCenter ">{res.price} <span>р.</span></div>
+                        </Card.Footer>
+                      </Card>
+                    </Col>
                   )
                 })
               }
             </Row>
           )}
           <Button variant="info" className="buttonCenter"><Link to="catalogs/iron2"
-            style={{ color: 'white', textDecoration: 'none' }}>увидеть
+                                                                style={{color: 'white', textDecoration: 'none'}}>увидеть
             больше</Link></Button>
         </CardDeck>
         <h2 className="prodHeader">Межкомнатные двери</h2>
@@ -84,31 +84,31 @@ const Items = (props) => {
               {
                 interiorDoor.map((res, index) => {
                   return (<Col xs={6} md={3} key={index}>
-                    <Card>
-                      <div className="flip-box">
-                        <div className="flip-box-inner">
+                      <Card>
+                        <div className="flip-box">
+                          <div className="">
 
-                          <div className="flip-box-back">
-                            <Card.Img variant="top" src={res.otherColor[0].image}
-                              style={{ height: '100%' }} />
+                            <div className="flip-box-back">
+                              <Card.Img variant="top" src={res.otherColor[0].image}
+                                        style={{height: '100%'}}/>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <Card.Body className="alignCenter">
-                        <Card.Title>{res.title}</Card.Title>
-                      </Card.Body>
-                      <Card.Footer>
-                        <div className="text-muted alignCenter ">{res.priceFront} <span>р.</span></div>
-                      </Card.Footer>
-                    </Card>
-                  </Col>
+                        <Card.Body className="alignCenter">
+                          <Card.Title>{res.title}</Card.Title>
+                        </Card.Body>
+                        <Card.Footer>
+                          <div className="text-muted alignCenter ">{res.priceFront} <span>р.</span></div>
+                        </Card.Footer>
+                      </Card>
+                    </Col>
                   )
                 })
               }
             </Row>
           )}
           <Button variant="info" className="buttonCenter"><Link to="catalogs/iron"
-            style={{ color: 'white', textDecoration: 'none' }}>увидеть
+                                                                style={{color: 'white', textDecoration: 'none'}}>увидеть
             больше</Link></Button>
         </CardDeck>
       </Container>
@@ -119,8 +119,8 @@ const Items = (props) => {
 
 const mapStateToProps = state => {
   return {
-    doors: state.doors
+    doors: state.doors,
   }
 }
 
-export default connect(mapStateToProps, { getInteriorDoors, getIronDoors })(Items)
+export default connect(mapStateToProps, {getInteriorDoors, getIronDoors})(Items)
