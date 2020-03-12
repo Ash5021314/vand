a6c204c34a9cf8c2ec741237c749f7ef3a7c6d67
Ash5021314 <ashkapoghosyan@gmail.com>
2020-03-12 10:50:13 +0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const app = require('./app')
 let port = process.env.PORT || 5000
+
+console.log('process.env.PORT', process.env.PORT)
+
 http.createServer(app).listen(port, () => {
   console.log(`Server running on port ${port}...`)
 })
