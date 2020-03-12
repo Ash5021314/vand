335ce7fa21b61ecc8d66a3db00eb716602a7ec3f
Ash5021314 <ashkapoghosyan@gmail.com>
2020-03-12 14:06:42 +0400

    fix

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
-let db = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+let db = process.env.MONGODB_URI
 let opts = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
-};
+  useFindAndModify: false,
+}
+
 mongoose.connect(db, opts, err => {
-  if (err) throw err;
-  console.log("DB connected ...");
-});
+  if (err) throw err
+  console.log('DB connected ...')
+})
--- src/routes/middleware.js
+++ src/routes/middleware.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const Admin = require("../models/Admin");
-const jwt = require("jsonwebtoken");
-const path = require("path");
-const { UNAUTHIORIZED } = require("../utils/response_constants");
-const { v4 } = require("uuid");
-const multer = require("multer");
+const express = require('express')
+const Admin = require('../models/Admin')
+const jwt = require('jsonwebtoken')
+const path = require('path')
+const { UNAUTHIORIZED } = require('../utils/response_constants')
+const { v4 } = require('uuid')
+const multer = require('multer')
 
 let validator = async (req, res, next) => {
   try {
-    let token = req.header("Authorization");
+    let token = req.header('Authorization')
     let tokenIsExist = await Admin.find({
-      login: "gago",
-      tokens: { $elemMatch: { token } }
-    });
+      login: 'gago',
+      tokens: { $elemMatch: { token } },
+    })
     if (tokenIsExist && tokenIsExist.length > 0) {
-      jwt.verify(token, process.env.JWT_SECRET);
-      next();
+      jwt.verify(token, process.env.JWT_SECRET)
+      next()
     } else {
-      return res.status(UNAUTHIORIZED.statusCode).send(UNAUTHIORIZED);
+      return res.status(UNAUTHIORIZED.statusCode).send(UNAUTHIORIZED)
     }
   } catch (e) {
-    return res.status(UNAUTHIORIZED.statusCode).send(UNAUTHIORIZED);
+    return res.status(UNAUTHIORIZED.statusCode).send(UNAUTHIORIZED)
   }
-};
+}
 
 let imageUploader = async (req, res, next) => {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "/../public", "/images/doors/"));
+      cb(null, path.join(__dirname, '/../public', '/images/doors/'))
     },
     filename: function (req, file, cb) {
-      cb(null, "IMAGE-" + v4().replace(/-/g, "") + ".jpg");
-    }
-  });
+      cb(null, 'IMAGE-' + v4().replace(/-/g, '') + '.jpg')
+    },
+  })
   multer({
     storage: storage,
-    limits: { fileSize: process.env.FILE_SIZE_LIMIT }
-  }).any();
+    limits: { fileSize: process.env.FILE_SIZE_LIMIT },
+  }).any()
   // next();
-};
+}
 module.exports = {
   validator,
-  imageUploader
-};
+  imageUploader,
+}
