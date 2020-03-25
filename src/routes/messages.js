5596a4ac27b448bd15e0cc012fa5ec96b4c5e4c8
Ash5021314 <ashkapoghosyan@gmail.com>
2020-03-25 23:04:54 +0400

    adds mailing system

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,44 +1,71 @@
-const express          = require('express')
-const router           = express.Router()
-const { messages }     = require('../providers')
-const { SERVER_ERROR } = require('../utils/response_constants')
+const express = require("express");
+const router = express.Router();
+const { messages } = require("../providers");
+const { SERVER_ERROR } = require("../utils/response_constants");
 
-router.get('/', async (req, res) => {
-    try {
-        const doc = await messages.get()
-        return res.status(doc.statusCode).send(doc)
-    } catch (e) {
-        return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR)
-    }
-})
+var MailConfig = require("./email-config");
+var hbs = require("nodemailer-express-handlebars");
+var gmailTransport = MailConfig.GmailTransport;
+router.get("/email/template", (req, res, next) => {});
 
-router.post('/', async (req, res) => {
-    try {
-        const doc = await messages.send(req.body)
-        return res.status(doc.statusCode).send(doc)
-    } catch (e) {
-        console.log(e)
-        return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR)
-    }
-})
+router.get("/", async (req, res) => {
+	try {
+		const doc = await messages.get();
+		return res.status(doc.statusCode).send(doc);
+	} catch (e) {
+		return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR);
+	}
+});
 
-router.patch('/:id', async (req, res) => {
-    try {
-        const doc = await messages.seen(req.params.id)
-        return res.status(doc.statusCode).send(doc)
-    } catch (e) {
-        return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR)
-    }
-})
+router.post("/", async (req, res) => {
+	try {
+		const doc = await messages.send(req.body);
+		if (200 == doc.statusCode) {
+			MailConfig.ViewOption(gmailTransport, hbs);
+			let HelperOptions = {
+				from: "Vandoors <vandoorsinfo@gmail.com>",
+				to: "ashkapoghosyan@gmail.com",
+				subject: "Nor patver!",
+				template: "test",
+				context: {
+					name: req.body.name,
+					phone: req.body.phone
+				}
+			};
+			gmailTransport.sendMail(HelperOptions, (error, info) => {
+				if (error) {
+					// console.log(error);
+					res.json(error);
+				}
+				// console.log("email is send");
+				// console.log(info);
+				res.json(info);
+			});
+		}
+		return res.status(doc.statusCode).send(doc);
+	} catch (e) {
+		console.log(e);
+		return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR);
+	}
+});
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const doc = await messages.delete(req.params.id)
-        return res.status(doc.statusCode).send(doc)
-    } catch (e) {
-        console.log(e)
-        return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR)
-    }
-})
+router.patch("/:id", async (req, res) => {
+	try {
+		const doc = await messages.seen(req.params.id);
+		return res.status(doc.statusCode).send(doc);
+	} catch (e) {
+		return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR);
+	}
+});
 
-module.exports = router
+router.delete("/:id", async (req, res) => {
+	try {
+		const doc = await messages.delete(req.params.id);
+		return res.status(doc.statusCode).send(doc);
+	} catch (e) {
+		console.log(e);
+		return res.status(SERVER_ERROR.statusCode).send(SERVER_ERROR);
+	}
+});
+
+module.exports = router;
